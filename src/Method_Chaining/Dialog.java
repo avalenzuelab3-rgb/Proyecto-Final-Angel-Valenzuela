331a9814cf0baf725b4a0b2ea115020a1c81c51a
Angel Valenzuela <avalenzuelab3@miumg.edu.gt>
2026-05-14 16:16:13 -0600

    Method_Chaoning

---
+++ src/Method_Chaining/Dialog.java
@@ -0,0 +1,16 @@
+package Method_Chaining;
+
+public class Dialog {
+
+    public Dialog() {
+
+    }
+
+    public void setTitle(String title) {
+
+    }
+    public void setMessage(String message) {   
+    }     
+    public void setPositiveButton() {
+    }
+}  
